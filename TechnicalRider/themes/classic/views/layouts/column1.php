c80cbed13de19cff5d0d584c0e270e980a431c1c
srodri31 <srodri31@eafit.edu.co>
2015-10-18 18:31:37 -0500

    eliminando anterior plantilla

--- TechnicalRider/themes/classic/views/layouts/column1.php
+++
@@ -1,8 +0,0 @@
-<?php /* @var $this Controller */ ?>
-<?php $this->beginContent('//layouts/main'); ?>
-<div class="container">
-	<div class="span12">
-		<?php echo $content; ?>
-	</div>
-</div><!-- content -->
-<?php $this->endContent(); ?>
